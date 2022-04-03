695a94ad574aa05810c666ad62655c66aabebf2f
Dzergo <dzergo.s@gmail.com>
2022-04-03 17:32:18 +0300

    Simple Http Server + readStreams

--- utils/Notifications.js
+++ utils/Notifications.js
@@ -1,20 +1,34 @@
 const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
 const EventEmitter = require("events");
-const {newEventLog} = require("./EventLogger");
+const logger = require("./Logger");
+const fsPromises = require("fs/promises");
 
-const notifications = new EventEmitter();
+class ExtEventEmitter extends EventEmitter {
+    constructor(options) {
+        super(options);
 
-const argv = yargs(hideBin(process.argv)).argv;
+        this.checkVerbose();
+    }
 
-async function notify (eventName, payload) {
-    if(argv.verbose)
-        await newEventLog(eventName, payload);
+    checkVerbose() {
+        const argv = yargs(hideBin(process.argv)).argv;
+        this._verbose = !!argv.verbose;
+        logger.warn(`[ExtEventEmitter] Verbose mode: ${!!this._verbose}`)
+    }
 
-    notifications.emit(eventName, payload);
+    async newEventLog (eventName, payload) {
+        await fsPromises.appendFile("./event.log", `---------[${new Date().toUTCString()}] [${eventName.toUpperCase()}]:\n ${payload}\n`);
+    }
+
+    emit(event, ...args) {
+        this._verbose && this.newEventLog(event, ...args);
+        return super.emit(event, ...args)
+    }
 }
 
+const notifications = new ExtEventEmitter();
+
 module.exports = {
-    notify,
     notifications
 }
